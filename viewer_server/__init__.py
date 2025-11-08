ef369f0195399ec7ed8fa92b6d62f1546b4bc959
nanashio <69329367+nanashio@users.noreply.github.com>
2025-11-08 10:12:58 +0900

    Restore legacy viewer server exports

--- viewer_server/__init__.py
+++ viewer_server/__init__.py
@@ -5,6 +5,9 @@ from .app import ServerContext, find_viewer, open_browser, resolve_results_dir
 from .handlers import API_SAVE_PATH, GalleryRequestHandler
 from .main import create_server, main
 
+_open_browser = open_browser
+_resolve_results_dir = resolve_results_dir
+
 __all__ = [
     "API_SAVE_PATH",
     "GalleryRequestHandler",
@@ -13,5 +16,7 @@ __all__ = [
     "find_viewer",
     "main",
     "open_browser",
+    "_open_browser",
     "resolve_results_dir",
+    "_resolve_results_dir",
 ]
